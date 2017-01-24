f3085f64c3ce4b4f957df48494e8b69d340f670c
Yueh Chou <yueh.k.chou@gmail.com>
2017-01-24 17:18:47 -0500

    package change

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,19 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/'
   },
+  devServer: {
+      inline: true,
+      port: 8448
+   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/
+        query: {
+          presets: ['react', 'es2015']
+        }
       }
     ]
   }
